f3aea99de1b6e23ff599610b4bd08571951c5fc5
Kanstantsin Lazouski <kochtaik@gmail.com>
2024-10-21 09:09:20 +0200

    Solve Trim

--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -1,1 +1,8 @@
-type Trim<S extends string> = any
+type Space = ' ' | '\t' | '\n'
+
+type TrimLeft_<S extends string> = S extends `${Space}${infer Base}` ? TrimLeft_<Base> : S
+type TrimRight_<S extends string> = S extends `${infer Base}${Space}` ? TrimRight_<Base> : S
+
+
+type Trim<S extends string> = TrimLeft_<TrimRight_<S>>
+
