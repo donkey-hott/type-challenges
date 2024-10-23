343ea0b5e6911258af65702e2f88e9807950e53c
kosta-dent <k.lazouski@tdsoftware.de>
2024-10-23 16:13:08 +0200

    solve Length Of String

--- questions/00298-medium-length-of-string/template.ts
+++ questions/00298-medium-length-of-string/template.ts
@@ -1,1 +1,8 @@
-type LengthOfString<S extends string> = any
+type StringToTuple<S extends string> =
+  S extends ''
+  ? []
+  : S extends `${infer First}${infer Rest}`
+  ? [First, ...StringToTuple<Rest>]
+  : never
+
+type LengthOfString<S extends string> = StringToTuple<S>['length']
