0ac329baeef49ce6c1096c302d011de57f607695
Kanstantsin Lazouski <kochtaik@gmail.com>
2024-10-26 13:52:20 +0200

    Solve Flatten

--- questions/00459-medium-flatten/template.ts
+++ questions/00459-medium-flatten/template.ts
@@ -1,1 +1,11 @@
-type Flatten = any
+type Flatten<T extends unknown[]> =
+  T extends [infer First, ...infer Rest]
+  ? [
+    ...(
+      First extends unknown[]
+      ? Flatten<First>
+      : [First]
+    ),
+    ...Flatten<Rest>
+  ]
+  : []
